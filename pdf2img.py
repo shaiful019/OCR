c663d6d929a184b6c14aaa87d0d18b3a3ca2b19a
shaiful <shaiful019@gmail.com>
2021-09-29 05:27:08 +0600

    File organization

--- pdf2img.py
+++
@@ -1,5 +0,0 @@
-from pdf2image import convert_from_path
-images = convert_from_path("norlite 4-22-21 b.pdf", 500)
-for i, image in enumerate(images):
-    fname = 'image'+str(i)+'.jpg'
-    image.save(fname, "JPEG")
